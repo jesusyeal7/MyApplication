3d1e36645cfe9a0d3f5de9a21d301f29e57a06e3
jesus <jyramos@miners.utep.edu>
2019-03-31 15:26:14 -0600

    This contribution is from Adrian Gomez Rodriguez
    Signed-off-by: jesus <jyramos@miners.utep.edu>

--- app/src/main/java/com/example/pricewatcher20/About.java
+++ app/src/main/java/com/example/pricewatcher20/About.java
@@ -4,7 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
-
+//This is a test
 public class About extends AppCompatActivity{
 
     @Override
